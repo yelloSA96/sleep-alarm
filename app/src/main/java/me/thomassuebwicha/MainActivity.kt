b681f4ae7d13382f0de39e205237ed81b566a2f6
yellosa96 <t.suebwicha@gmail.com>
2025-12-29 18:58:08 +1100

    SA-2: Clean up application

--- app/src/main/java/me/thomassuebwicha/MainActivity.kt
+++ app/src/main/java/me/thomassuebwicha/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import me.thomassuebwicha.ui.main.SleepAlarmActivity
 import me.thomassuebwicha.ui.theme.MyApplicationTheme
 
@@ -34,42 +31,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.DarkGray) {
-        Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier,
-            color = Color.White
-        )
-    }
-}
-
-@Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-
-    )
-}
-
-@Composable
-fun NewSection() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Surface {
-            Text(
-                text = "This is another text line",
-                fontSize = 100.sp
-            )
-        }
-    }
-}
-
-
 
 @Preview(showBackground = true,
     )
