415a7b2e2c59654ea71d245a6edf180dcfe171e0
yellosa96 <t.suebwicha@gmail.com>
2025-12-29 16:43:36 +1100

    SA-2: Temp Fix

--- app/src/main/java/me/thomassuebwicha/Logic.kt
+++ app/src/main/java/me/thomassuebwicha/Logic.kt
@@ -4,15 +4,27 @@ package me.thomassuebwicha;
 import android.content.Context
 import android.content.Intent;
 import android.provider.AlarmClock;
+import android.widget.Toast
 
 
 fun setAlarm(context: Context, hour: Int, minute: Int, message: String) {
-    val intent = Intent(AlarmClock.ACTION_SET_ALARM)
-    intent.putExtra(AlarmClock.EXTRA_HOUR, hour)
-    intent.putExtra(AlarmClock.EXTRA_MINUTES, minute)
-    intent.putExtra(AlarmClock.EXTRA_MESSAGE, message)
-
-    if (intent.resolveActivity(context.packageManager) != null) {
-        context.startActivity(intent)
+    try {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minute)
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, false) // Shows the Clock app UI
+        }
+        android.util.Log.i("SettingAlarm",intent.toString())
+        // Check if there's an app that can handle this intent
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        } else {
+            android.util.Log.e("SettingAlarm","No clock app found.")
+            Toast.makeText(context, "No clock app found", Toast.LENGTH_SHORT).show()
+        }
+    } catch (e: Exception) {
+        android.util.Log.e("SettingAlarm","Error setting alarm")
+        Toast.makeText(context, "Error setting alarm: ${e.message}", Toast.LENGTH_SHORT).show()
     }
 }
