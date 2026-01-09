5b3b321baab6891295e408d5e4b8557953b54b36
yellosa96 <t.suebwicha@gmail.com>
2026-01-09 20:50:57 +1100

    SA-6: Updating Color Theme

--- app/src/main/java/me/thomassuebwicha/ui/theme/Theme.kt
+++ app/src/main/java/me/thomassuebwicha/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -18,8 +19,8 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = Color(0xFF6B4FA0), // Purple color for button
+    secondaryContainer = Color(0xFFE8F5E9), // Light green for selected tab
     tertiary = Pink40
 
     /* Other default colors to override
