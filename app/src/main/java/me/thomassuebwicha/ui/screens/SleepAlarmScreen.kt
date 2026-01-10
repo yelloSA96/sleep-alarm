57439c418411682e6d12fdd4486889c2bacae0c8
yellosa96 <t.suebwicha@gmail.com>
2026-01-10 21:06:41 +1100

    SA-6: Adding showTimePicker modal

--- app/src/main/java/me/thomassuebwicha/ui/screens/SleepAlarmScreen.kt
+++ app/src/main/java/me/thomassuebwicha/ui/screens/SleepAlarmScreen.kt
@@ -1,5 +1,6 @@
 package me.thomassuebwicha.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,11 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -24,11 +30,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import me.thomassuebwicha.ui.tester.TimePickerDialog
 import me.thomassuebwicha.viewmodels.setAlarm
 import java.util.Calendar
 import java.util.Locale
 
-// TODO: Improve this composable
 @Composable
 fun SleepAlarmScreen(
     navController: NavController,
@@ -36,40 +42,26 @@ fun SleepAlarmScreen(
 ) {
     val context = LocalContext.current
     val calendar = Calendar.getInstance()
-    var hour by remember { mutableIntStateOf(calendar.get(Calendar.HOUR_OF_DAY)) }
-    var minute by remember { mutableIntStateOf(calendar.get(Calendar.MINUTE)) }
-    val timeString = String.format(Locale.ENGLISH, "%02d:%02d", hour, minute)
+    var displayableHour by remember { mutableIntStateOf(calendar.get(Calendar.HOUR_OF_DAY)) }
+    var displayableMinute by remember { mutableIntStateOf(calendar.get(Calendar.MINUTE)) }
+    var selectedTime by remember { mutableStateOf<Calendar?>(null) }
+    var showTimePicker by remember { mutableStateOf(false) }
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
         Text(
-            text = timeString,
-            fontSize = 58.sp,
+            text = "Select your wake up time",
+            fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground
         )
 
         Spacer(modifier = Modifier.height(40.dp))
 
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Button(
-                onClick = {
-                    val currentCalendar = Calendar.getInstance()
-                    hour = currentCalendar.get(Calendar.HOUR_OF_DAY)
-                    minute = currentCalendar.get(Calendar.MINUTE)
-                },
-                modifier = Modifier.weight(1f)
-            ) {
-                Text("Reset Time")
-            }
-        }
+        Spacer(modifier = Modifier.height(40.dp))
 
         // Grid for buttons
         Row(
@@ -80,9 +72,9 @@ fun SleepAlarmScreen(
         ) {
             Button(
                 onClick = {
-                    val totalMinutes = hour * 60 + minute + 90
-                    hour = (totalMinutes / 60) % 24
-                    minute = totalMinutes % 60
+                    val totalMinutes = displayableHour * 60 + displayableMinute + 90
+                    displayableHour = (totalMinutes / 60) % 24
+                    displayableMinute = totalMinutes % 60
                 },
                 modifier = Modifier.weight(1f)
             ) {
@@ -91,7 +83,7 @@ fun SleepAlarmScreen(
 
             Button(
                 onClick = {
-                    hour = (hour + 3) % 24
+                    displayableHour = (displayableHour + 3) % 24
                 },
                 modifier = Modifier.weight(1f)
             ) {
@@ -107,9 +99,9 @@ fun SleepAlarmScreen(
         ) {
             Button(
                 onClick = {
-                    val totalMinutes = hour * 60 + minute + 270
-                    hour = (totalMinutes / 60) % 24
-                    minute = totalMinutes % 60
+                    val totalMinutes = displayableHour * 60 + displayableMinute + 270
+                    displayableHour = (totalMinutes / 60) % 24
+                    displayableMinute = totalMinutes % 60
                 },
                 modifier = Modifier.weight(1f)
             ) {
@@ -118,7 +110,7 @@ fun SleepAlarmScreen(
 
             Button(
                 onClick = {
-                    hour = (hour + 6) % 24
+                    displayableHour = (displayableHour + 6) % 24
                 },
                 modifier = Modifier.weight(1f)
             ) {
@@ -134,9 +126,9 @@ fun SleepAlarmScreen(
         ) {
             Button(
                 onClick = {
-                    val totalMinutes = hour * 60 + minute + 450
-                    hour = (totalMinutes / 60) % 24
-                    minute = totalMinutes % 60
+                    val totalMinutes = displayableHour * 60 + displayableMinute + 450
+                    displayableHour = (totalMinutes / 60) % 24
+                    displayableMinute = totalMinutes % 60
                 },
                 modifier = Modifier.weight(1f)
             ) {
@@ -145,12 +137,31 @@ fun SleepAlarmScreen(
 
             Button(
                 onClick = {
-                    hour = (hour + 9) % 24
+                    displayableHour = (displayableHour + 9) % 24
                 },
                 modifier = Modifier.weight(1f)
             ) {
                 Text("+9 Hr")
             }
+        }
+
+        Button(
+            colors = ButtonColors(
+                containerColor = MaterialTheme.colorScheme.background,
+                disabledContainerColor = MaterialTheme.colorScheme.background,
+                contentColor = MaterialTheme.colorScheme.onBackground,
+                disabledContentColor = MaterialTheme.colorScheme.onBackground
+            ),
+            onClick = {
+                showTimePicker = true
+            },
+        ) {
+            Text(
+                text = String.format(Locale.ENGLISH, "%02d:%02d", displayableHour, displayableMinute),
+                fontSize = 58.sp,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+
         }
 
         Row(
@@ -161,16 +172,58 @@ fun SleepAlarmScreen(
         ) {
             Button(
                 onClick = {
-                    setAlarm(context, hour, minute, "Made by Sleep Application!")
+                    setAlarm(context, displayableHour, displayableMinute, "Made by Sleep Application!")
                 },
                 modifier = Modifier.weight(1f)
             ) {
-                Text("Set Alarm")
+                Text("Set Sleep Alarm")
             }
+        }
+
+
+        if (showTimePicker) {
+            Log.i("TimePicker", "showTimePicker is true, showing TimePickerDialog")
+            TimePickerDialog(
+                onDismissRequest = { showTimePicker = false },
+                onConfirm = { hour, minute ->
+                    displayableHour = hour
+                    displayableMinute = minute
+                    showTimePicker = false
+                }
+            )
         }
 
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DialPickerWidget(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
+
+    Column {
+        TimePicker(
+            state = timePickerState,
+        )
+        Button(onClick = onDismiss) {
+            Text("Dismis Picker")
+        }
+        Button(onClick = onConfirm) {
+            Text("Confirm selection")
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
