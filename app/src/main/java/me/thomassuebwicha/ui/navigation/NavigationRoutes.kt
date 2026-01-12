102c4a6531e6b884588c8d2da5830aab97e7a704
yellosa96 <t.suebwicha@gmail.com>
2026-01-13 08:08:49 +1100

    SA-6: Clean Up

--- app/src/main/java/me/thomassuebwicha/ui/navigation/NavigationRoutes.kt
+++ app/src/main/java/me/thomassuebwicha/ui/navigation/NavigationRoutes.kt
@@ -1,8 +1,6 @@
 package me.thomassuebwicha.ui.navigation
 
-//import androidx.compose.ui.res.stringResource
-//import me.thomassuebwicha.R
-//
+
 object NavigationRoutes {
     const val HOME = "selection_screen"
     const val ALARM_SCREEN = "alarm_screen"
