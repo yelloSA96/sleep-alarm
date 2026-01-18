aa910be2fcdf53696b6ecc1661edf65c03d876b2
yellosa96 <t.suebwicha@gmail.com>
2026-01-18 13:27:28 +1100

    sa-10: Adding kdocs

--- app/src/main/java/me/thomassuebwicha/util/TimeUtils.kt
+++ app/src/main/java/me/thomassuebwicha/util/TimeUtils.kt
@@ -1,5 +1,13 @@
 package me.thomassuebwicha.util
 
+/**
+ * Subtracts a specified number of minutes from a given time represented by hour and minute.
+ *
+ * @param hour The hour component of the time (0-23).
+ * @param minute The minute component of the time (0-59).
+ * @param minutesToSubtract The number of minutes to subtract.
+ * @return A Pair containing the new hour and minute after subtraction.
+ */
 fun subtractMinutesFromTime(hour: Int, minute: Int, minutesToSubtract: Int): Pair<Int, Int> {
     val totalMinutes = hour * 60 + minute - minutesToSubtract
     val newHour = ((totalMinutes / 60) % 24 + 24) % 24
